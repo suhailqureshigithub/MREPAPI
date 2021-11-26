66f24c8f197a90b4eaa232cce2e9659c743c312d
suhailqureshigithub <muhammad.suhail@iblgrp.com>
2021-11-26 12:14:00 +0500

    commit

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app=FastAPI()
 
-@app.get('/')
-def index():
-    return{'data': 'Blog List'}
+@app.get('/blog')
+def index(limit=100,published:bool =True,sort:Optional[str]=None):
+
+    if published:
+        return{'data': f'{limit} published blog(s) from DB'}
+    else:
+        return{'data': f'All blog(s) from DB'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return{'List of all unpublished blog':'All'}
 
 @app.get('/blog/{id}')
-def show(id):
+def show(id:int):
     return{'blog id':id}
 
-
 @app.get('/blog/{id}/comments')
-def show(id):
+def show(id:int):
     return{'blog id with comments':{'1','2'}}
